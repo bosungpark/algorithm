c4ec171f34f298d235d65d49c664b7a64e25d1b5
bosung park <81157873+bosungpark@users.noreply.github.com>
2022-05-27 13:20:51 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 6, Time: 892 ms, Memory: 85216 KB -BaekjoonHub

---
+++ 백준/Silver/24484. 알고리즘 수업 － 깊이 우선 탐색 6/알고리즘 수업 － 깊이 우선 탐색 6.py
@@ -0,0 +1,25 @@
+import sys
+from collections import defaultdict
+
+N,M,R=map(int, sys.stdin.readline().split())
+graph=defaultdict(list)
+tree=dict()
+for _ in range(M):
+    u,v=map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+stack=[(R,0)]
+order=1
+while stack:
+    node, depth=stack.pop()
+    if node not in tree:
+        tree[node]=(depth,order)
+        order+=1
+        for n in sorted(graph[node],reverse=False):
+            stack.append((n,depth+1))
+sum=0
+for depth, order in tree.values():
+    sum+=depth*order
+print(sum)
